5f59b03c7b53de8429120c81281c73005e2c3b61
Joseph Olympia <60299300+jokyon@users.noreply.github.com>
2020-12-02 18:23:19 -0800

    changing game id and testing api features

---
+++ CoreApp/consts.ts
@@ -0,0 +1,39 @@
+const fortniteClassId = 5426;
+
+const interestingFeatures = [
+  
+  'live_client_data',
+  'matchState',
+  'match_info',
+  'death',
+  'respawn',
+  'abilities',
+  'kill',
+  'assist',
+  'gold',
+  'minions',
+  'summoner_info',
+  'gameMode',
+  'teams',
+  'level',
+  'announcer',
+  'counters',
+  'damage',
+  'heal',
+];
+
+const windowNames = {
+  inGame: 'in_game',
+  desktop: 'desktop'
+};
+
+const hotkeys = {
+  toggle: 'showhide'
+};
+
+export {
+  fortniteClassId,
+  interestingFeatures,
+  windowNames,
+  hotkeys
+}
